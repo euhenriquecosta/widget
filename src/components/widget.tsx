8295fba6f759b4a8e9e0924a2d0328570e4813d8
guialveess <97guilherme.alves@gmail.com>
2025-07-25 12:16:51 -0300

    feat: update iframe with chatview 2

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -24,7 +24,7 @@ export function Widget({ config }: WidgetProps) {
   const height = config.height ?? 600;
 
   // 🚀 se quiser trocar o flowId, basta passar aqui dinamicamente
-  const flowId = '6882cdb9928d72c6536c71a4';
+  const flowId = '688389642e4b04b550bc269c';
   const iframeUrl = `http://localhost:5173/widgets?theme=${theme}&flowId=${flowId}`;
 
   const iframeRef = useRef<HTMLIFrameElement>(null);
